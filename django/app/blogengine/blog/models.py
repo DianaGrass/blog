4581621fed88fa16ebae79c096226a20d395ea22
DianaGrass <di948588@gmail.com>
2020-01-11 23:48:37 +0300

    Initial commit

---
+++ django/app/blogengine/blog/models.py
@@ -0,0 +1,53 @@
+from django.db import models
+from django.shortcuts import reverse
+
+from django.utils.text import slugify
+from time import time
+
+
+def gen_slug(s):
+    new_slug = slugify(s, allow_unicode=True)
+    return new_slug + '-' + str(int(time()))
+
+class Post(models.Model):
+    title = models.CharField(max_length=150, db_index=True)
+    # Человекопонятный  url, флаг db_index=True не нужен, т.к. поля
+    # где unique=True индексируются автоматически
+    slug = models.SlugField(max_length=150, blank=True, unique=True)
+    # db_index=True - флаг на индексацию, чтобы можно было делать
+    # быстрый поиск по содержанию
+    body = models.TextField(blank=True, db_index=True)
+    tags = models.ManyToManyField('Tag', blank=True, related_name='posts')
+    date_pub = models.DateTimeField(auto_now_add=True)
+
+    def get_absolute_url(self):
+        return reverse('post_detail_url', kwargs={'slug': self.slug})
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = gen_slug(self.title)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+        # return '{}'.format(self.title)
+
+class Tag(models.Model):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=150, unique=True)
+
+    def get_absolute_url(self):
+        return reverse('tag_detail_url', kwargs={'slug': self.slug})
+
+    def __str__(self):
+        # return '{}'.format(self.title)
+        return self.title
+
+
+# class Tag(models.Model):
+#     title = models.CharField(max_length=50)
+#     slug = models.SlugField(default='t0', max_length=50,
+#                             unique=False, db_index=True)
+#
+#     def __str__(self):
+#         return '{}'.format(self.title)
---
+++ django/app/blogengine/blog/views.py
@@ -0,0 +1,65 @@
+from django.shortcuts import render
+from django.shortcuts import redirect
+from django.shortcuts import get_object_or_404
+from django.views.generic import View
+#from django.http import HttpResponse
+
+from .models import Post, Tag
+from .utils import *
+from .forms import TagForm, PostForm
+
+
+def posts_list(request):
+    #return HttpResponse('<h1>Hello world</h1>')
+    posts = Post.objects.all()
+    return render(request, 'blog/index.html', context={'posts': posts})
+
+
+class PostDetail(ObjectDetailMixin, View):
+    model  = Post
+    template = 'blog/post_detail.html'
+
+
+class PostCreate(ObjectCreateMixin, View):
+    model_form = PostForm
+    template = 'blog/post_create_form.html'
+
+    # def get(self, request):
+    #     form = PostForm()
+    #     return render(request, 'blog/post_create_form.html', context={'form': form})
+    #
+    # def post(self, request):
+    #     bound_form = PostForm(request.POST)
+    #     if bound_form.is_valid():
+    #         new_post = bound_form.save()
+    #         return redirect(new_post)
+    #     return render(request, 'blog/post_create.html', context={'form': bound_form})
+
+
+
+
+
+class TagDetail(ObjectDetailMixin, View):
+    model = Tag
+    template = 'blog/tag_detail.html'
+
+
+class TagCreate(ObjectCreateMixin, View):
+    model_form = TagForm
+    template = 'blog/tag_create.html'
+    # def get(self, request):
+    #     form = TagForm()
+    #     return render(request, 'blog/tag_create.html', context={'form': form})
+    #
+    # def post(self, request):
+    #     bound_form = TagForm(request.POST)
+    #
+    #     if bound_form.is_valid():
+    #         new_tag = bound_form.save()
+    #         return redirect(new_tag)
+    #     return render(request, 'blog/tag_create.html', context={'form': bound_form})
+
+
+def tags_list(request):
+    tags = Tag.objects.all()
+    return render(request, 'blog/tags_list.html', context={'tags': tags})
