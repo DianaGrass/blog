ec2fa8a5bb1f43eb183f55519fa581ac80673f38
DianaGrass <di848588@gmail.com>
2020-05-14 01:32:05 +0300

    refactoring

--- django/app/blogengine/blog/models.py
+++ django/app/blogengine/blog/models.py
@@ -27,6 +27,9 @@ class Post(models.Model):
     def get_update_url(self):
         return reverse('post_update_url', kwargs={'slug': self.slug})
 
+    def get_delete_url(self):
+        return reverse('post_delete_url', kwargs={'slug': self.slug})
+
     def save(self, *args, **kwargs):
         if not self.id:
             self.slug = gen_slug(self.title)
@@ -50,6 +53,9 @@ class Tag(models.Model):
     def __str__(self):
         # return '{}'.format(self.title)
         return self.title
+
+    def get_delete_url(self):
+        return reverse('tag_delete_url', kwargs={'slug': self.slug})
 
 
 # class Tag(models.Model):
