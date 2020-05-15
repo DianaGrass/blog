c364f8ba3a8b3c45d21d93f4d69bb184549b508e
DianaGrass <di848588@gmail.com>
2020-05-15 22:54:14 +0300

    add mixin for delete

--- django/app/blogengine/blog/views.py
+++ django/app/blogengine/blog/views.py
@@ -82,15 +82,19 @@ class TagUpdate(ObjectUpdateMixin, View):
     #     return render(request, 'blog/tag_update_form', context={'form': bound_form, 'tag': tag})
 
 
-class TagDelete(View):
-    def get(self, request, slug):
-        tag = Tag.objects.get(slug__iexact=slug)
-        return render(request, 'blog/tag_delete_form.html', context={'tag': tag})
+class TagDelete(ObjectDeleteMixin, View):
+    model = Tag
+    template = 'blog/tag_delete_form.html'
+    redirect_url = 'tags_list_url'
 
-    def post(self, request, slug):
-        tag = Tag.objects.get(slug__iexact=slug)
-        tag.delete()
-        return redirect(reverse('tags_list_url'))
+    # def get(self, request, slug):
+    #     tag = Tag.objects.get(slug__iexact=slug)
+    #     return render(request, 'blog/tag_delete_form.html', context={'tag': tag})
+    #
+    # def post(self, request, slug):
+    #     tag = Tag.objects.get(slug__iexact=slug)
+    #     tag.delete()
+    #     return redirect(reverse('tags_list_url'))
 
 
 def tags_list(request):
