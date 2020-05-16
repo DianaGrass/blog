895157e22e8769e6562d191e6b826bbc9e517a30
DianaGrass <di848588@gmail.com>
2020-05-16 23:50:48 +0300

    redirect

--- django/app/blogengine/blogengine/urls.py
+++ django/app/blogengine/blogengine/urls.py
@@ -16,12 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
-
-#from .views import hello
+from .views import redirect_blog
 
 urlpatterns = [
+    path('', redirect_blog),
     path('admin/', admin.site.urls),
-    #Запросы по адресу /blog будут обрабатываться функцией hello
-    #Включили более детальную маршрутизацию из модуля urls.py приложения blog
     path('blog/', include('blog.urls'))
 ]
