16d58850a36a5952249e892551e0fb914b6c5b15
inity8 <di848588@gmail.com>
2020-05-21 23:39:17 +0300

    previous_page

--- django/app/blogengine/blog/views.py
+++ django/app/blogengine/blog/views.py
@@ -18,7 +18,28 @@ def posts_list(request):
     page_number = request.GET.get('page', 1)  #http://localhost:5000/blog/?page=2
     page = paginator.get_page(page_number)
 
-    return render(request, 'blog/index.html', context={'page_object': page})
+    is_paginated = page.has_other_pages()
+
+    if page.has_other_pages():
+        prev_url = '?page={}'.format(page.previous_page_number())
+
+    else:
+        prev_url = ''
+
+    if page.has_next():
+        next_url = '?page={}'.format(page.next_page_number())
+    else:
+        next_url = ''
+
+    context = {
+        'page_object': page,
+        'is_paginated': is_paginated,
+        'next_url': next_url,
+        'prev_url': prev_url,
+    }
+
+
+    return render(request, 'blog/index.html', context=context)
 
 
 class PostDetail(ObjectDetailMixin, View):
